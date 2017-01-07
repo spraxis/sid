f447068826ae913267ac49834343e7175f38fe20
Sebastian De Cicco <sebastian@praxisis.com>
2017-01-07 14:59:23 -0300

    Update the name of the module

--- Model/Sid.php
+++ Model/Sid.php
@@ -1,9 +1,9 @@
 <?php
-namespace Praxisis\ConsoleTools\Model;
+namespace Sebas\Sid\Model;
 
-use \Symfony\Component\Console\Command\Command;
-use \Symfony\Component\Console\Input\InputInterface;
-use \Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 
--- registration.php
+++ registration.php
@@ -1,7 +1,6 @@
 <?php
-
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Praxisis_ConsoleTools',
+    'Sebas_Sid',
     __DIR__
 );
